c9648ee68392d5b7b2fbee348019026f8109bde0
c-atarella <d454d@web.de>
2017-01-01 16:35:11 +0100

    Added missing tests.

--- gelf_test.go
+++ gelf_test.go
@@ -28,6 +28,13 @@ func TestZapLevelToGelfLevel(t *testing.T) {
 	}{
 		{name: "invalid level", args: args{l: -2}, want: 1},
 		{name: "debug level", args: args{l: -1}, want: 7},
+		{name: "info level", args: args{l: 0}, want: 6},
+		{name: "warn level", args: args{l: 1}, want: 4},
+		{name: "error level", args: args{l: 2}, want: 3},
+		{name: "dpanic level", args: args{l: 3}, want: 0},
+		{name: "panic level", args: args{l: 4}, want: 0},
+		{name: "fatal level", args: args{l: 5}, want: 0},
+		{name: "invalid level", args: args{l: 6}, want: 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
